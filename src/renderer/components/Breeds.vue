9bf9f4858db25e0d8be313a5f464c6862bf37869
PilarCha <pchavez92@gmail.com>
2020-05-10 22:38:04 -0700

    started styling the sections

--- src/renderer/components/Breeds.vue
+++ src/renderer/components/Breeds.vue
@@ -2,7 +2,13 @@
   <section>
     <h2>Home</h2>
     <div class="breeds">
-      <div class="breed">
+      <div class="breed" v-for='breed in breeds' :key='breed.id'>
+        <div class="breed-content">
+          <p><strong>Name:</strong> {{ breed.name }}</p> <br>
+          <p v-if="breed.origin"><strong>Origin:</strong> {{ breed.origin }}</p> <br>
+          <p v-if="breed.life_span"><strong>Life Span:</strong> {{ breed.life_span }}</p> <br>
+          <p v-if="breed.breed_group"><strong>Breed Group:</strong> {{ breed.breed_group }}</p> <br>
+        </div>
       </div>
     </div>
   </section>
@@ -35,6 +41,19 @@
   }
 </script>
 
-<style>
+<style lang='scss'>
+  .breeds {
+    display: grid;
+    grid-template-columns: repeat(3,1fr);
+    grid-gap:15px;
+    margin: 15px 0px;
 
+    @media screen and (max-width:600px){
+      grid-template-columns: repeat(1,1ft);
+    }
+    .breed {
+      background-color:#FFF;
+      box-shadow: 0px 3px 6px 0px rgba(0,0,0,0.2);
+    }
+  }
 </style>
