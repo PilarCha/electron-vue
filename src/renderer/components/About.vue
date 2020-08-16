5e80642d0339050fc85c5f85179c3516b6054c46
PilarCha <pchavez92@gmail.com>
2020-08-16 15:41:18 -0700

    Finished up the project

--- src/renderer/components/About.vue
+++ src/renderer/components/About.vue
@@ -1,7 +1,9 @@
 <template>
   <section>
     <h2>About</h2>
+    <p>You ever come back to a project a don't understand what is going on? That is what happened to me right now lol</p>
   </section>
+
 
 </template>
 
